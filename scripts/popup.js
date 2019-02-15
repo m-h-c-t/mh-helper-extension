bf657637af48fcc94ab46e1ae8f1937fe4f36102
tehhowch <tehhowchmh@gmail.com>
2019-02-15 09:02:10 -0600

    Update `findOpenMHTab`
     - Rename parameter "button_pressed"
       - is not a button, just an ID
     - Reorder parameters
       - Callback is required, but button ID may not be even consumed by callback
    
     - Update related usage / naming

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,14 +1,14 @@
 // JS script available only within the pop-up html pages (popup.html & popup2.html)
 /**
  * Query the open tabs and locate the MH tabs. Passes the first result to the callback, along with the invoking button.
- * @param {string} button_pressed the HTML id of the pressed button, to be forwarded to callback
  * @param {Function} callback A function that expects the MH page's tab ID and possibly the button that invoked the call
+ * @param {string} [button_id] the HTML id of the pressed button, to be forwarded to callback
  * @param {boolean} [silent] if true, errors will not be displayed to the user.
  */
-function findOpenMHTab(button_pressed, callback, silent) {
+function findOpenMHTab(callback, button_id, silent) {
     chrome.tabs.query({'url': ['*://www.mousehuntgame.com/*', '*://apps.facebook.com/mousehunt/*']}, tabs => {
         if (tabs.length > 0) {
-            callback(tabs[0].id, button_pressed);
+            callback(tabs[0].id, button_id);
         }
         else if (!silent) {
             displayErrorPopup("Please navigate to MouseHunt page first.");
@@ -20,17 +20,17 @@ function findOpenMHTab(button_pressed, callback, silent) {
  * Forward the pressed button to the content script on the identified tab.
  * (Horn button clicks also activate the MH page.)
  * @param {number} tab_id The tab ID of the MH page
- * @param {string} button_pressed The HTML element ID of the button that was clicked
+ * @param {string} button_id The HTML element ID of the button that was clicked
  */
-function sendMessageToScript(tab_id, button_pressed) {
+function sendMessageToScript(tab_id, button_id) {
     // Switch to MH tab if needed.
     let needsMHPageActive = ['horn', 'tsitu_loader', 'mhmh', 'ryonn'];
-    if (needsMHPageActive.includes(button_pressed)) {
+    if (needsMHPageActive.includes(button_id)) {
         chrome.tabs.update(tab_id, {'active': true});
     }
 
     // Send message to content script
-    chrome.tabs.sendMessage(tab_id, {jacks_link: button_pressed});
+    chrome.tabs.sendMessage(tab_id, {jacks_link: button_id});
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -38,13 +38,13 @@ document.addEventListener('DOMContentLoaded', () => {
     if (version_element) {
         version_element.innerText = ' v' + chrome.runtime.getManifest().version;
     }
-    findOpenMHTab("huntTimer", updateHuntTimer, true);
+    findOpenMHTab(updateHuntTimer, "huntTimer", true);
 
     // Send specific clicks to the content script for handling and/or additional forwarding.
     ['mhmh', 'userhistory', 'ryonn', 'horn', 'tsitu_loader'].forEach(id => {
         let button_element = document.getElementById(id);
         if (button_element) {
-            button_element.addEventListener('click', () => findOpenMHTab(id, sendMessageToScript));
+            button_element.addEventListener('click', () => findOpenMHTab(sendMessageToScript, id));
         }
     });
 });
