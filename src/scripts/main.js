ed4062d8364b26fe60118d42d20788a3c441cb8a
tehhowch <tehhowch@users.noreply.github.com>
2019-06-19 07:24:30 -0500

    Add Fiery Warpath "hunt details" (#40)
     - Report remaining mouse counts per wave, & if bosses are invincible.
     - Report `morale` / `support_morale` fields, for Warpath Thrasher computations
     - Report streak count, type, and whether the hunt extended it

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1398,6 +1398,7 @@
     const location_huntdetails_lookup = {
         "Bristle Woods Rift": addBristleWoodsRiftHuntDetails,
         "Claw Shot City": addClawShotCityHuntDetails,
+        "Fiery Warpath": addFieryWarpathHuntDetails,
         "Fort Rox": addFortRoxHuntDetails,
         "Harbour": addHarbourHuntDetails,
         "Sand Crypts": addSandCryptsHuntDetails,
@@ -1503,6 +1504,81 @@
                 at_boss: (map.remaining === 1)
             };
         }
+    }
+
+    /**
+     * Log the mouse populations, remaining total, boss invincibility, and streak data.
+     * MAYBE: Record usage of FW charms, e.g. "targeted mouse was attracted"
+     * charm_ids 534: Archer, 535: Cavalry, 536: Commander, 537: Mage, 538: Scout, 539: Warrior
+     *   540: S Archer, 541: S Cavalry, 542: S Mage, 543: S Scout, 544: S Warrior, 615: S Commander
+     * @param {Object <string, any>} message The message to be sent.
+     * @param {Object <string, any>} user The user state object, when the hunt was invoked (pre-hunt).
+     * @param {Object <string, any>} user_post The user state object, after the hunt.
+     * @param {Object <string, any>} hunt The journal entry corresponding to the active hunt.
+     */
+    function addFieryWarpathHuntDetails(message, user, user_post, hunt) {
+        const attrs = user.viewing_atts.desert_warpath;
+        const fw = {};
+        if ([1, 2, 3].includes(parseInt(attrs.wave, 10))) {
+            const asType = name => name.replace(/desert_|_weak|_epic|_strong/g, "");
+
+            if (attrs.streak_quantity > 0) {
+                fw.streak = {
+                    count: parseInt(attrs.streak_quantity, 10),
+                    type: asType(attrs.streak_type),
+                };
+                fw.streak.increased_on_hunt = (message.caught === 1 &&
+                    fw.streak.type === asType(user_post.viewing_atts.desert_warpath.streak_type));
+            }
+
+            // Track the mice remaining in the wave, per type and in total.
+            let remaining = 0;
+            [
+                "desert_warrior",
+                "desert_warrior_weak",
+                "desert_warrior_epic",
+                "desert_scout",
+                "desert_scout_weak",
+                "desert_scout_epic",
+                "desert_archer",
+                "desert_archer_weak",
+                "desert_archer_epic",
+                "desert_mage",
+                "desert_mage_strong",
+                "desert_cavalry",
+                "desert_cavalry_strong",
+                "desert_artillery"
+            ].filter(name => name in attrs.mice).forEach(mouse => {
+                const q = parseInt(attrs.mice[mouse].quantity, 10);
+                fw[`num_${asType(mouse)}`] = q;
+                remaining += q;
+            });
+            const wave_total = ({1: 105, 2: 185, 3: 260})[attrs.wave];
+            // Support retreats when 10% or fewer total mice remain.
+            fw.morale = remaining / wave_total;
+
+            fw.has_support_mice = (attrs.has_support_mice === "active");
+            if (fw.has_support_mice) {
+                // Calculate the non-rounded `morale_percent` viewing attribute.
+                fw.support_morale = (wave_total - remaining) / (.9 * wave_total);
+            }
+        } else if ([4, "4", "portal"].includes(attrs.wave)) {
+            // If the Warmonger or Artillery Commander was already caught (i.e. Ultimate Charm),
+            // don't record any hunt details since there isn't anything to learn.
+            const boss = attrs.mice[(message.stage === "Portal") ?
+                    "desert_artillery_commander" : "desert_boss"];
+            if (parseInt(boss.quantity, 10) !== 1) {
+                return;
+            }
+            // Theurgy Wardens are "desert_elite_gaurd". Yes, "gaurd".
+            fw.num_warden = parseInt(attrs.mice.desert_elite_gaurd.quantity, 10);
+            fw.boss_invincible = !!fw.num_warden;
+        } else {
+            if (debug_logging) {window.console.warn({record: message, user, user_post, hunt});}
+            throw new Error(`Unknown FW Wave "${attrs.wave}"`);
+        }
+
+        message.hunt_details = fw;
     }
 
     /**
