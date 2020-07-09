59e8a754f972cdbb037b74a6f6a362a602337d76
Tran Situ <trancsitu@gmail.com>
2020-07-08 17:57:26 -0700

    Fixed Valour Rift hunt details logic
    - Also set aside augment tracking due to lack of a compelling use case

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1903,11 +1903,13 @@
     function addValourRiftHuntDetails(message, user, user_post, hunt) {
         const attrs = user.environment_atts || user.enviroment_atts;
         // active_augmentations is undefined outside of the tower
-        if (attrs.stage === "tower") {
+        if (attrs.state === "tower") {
             message.hunt_details = {
-                super_siphon: !!attrs.active_augmentations.ss, // active = true, inactive = false
-                string_stepping: !!attrs.active_augmentations.sste,
                 floor: attrs.floor, // exact floor number (can be used to derive prestige and floor_type)
+                // No compelling use case for the following 3 augments at the moment
+                // super_siphon: !!attrs.active_augmentations.ss, // active = true, inactive = false
+                // string_stepping: !!attrs.active_augmentations.sste,
+                // elixir_rain: !!attrs.active_augmentations.er,
             };
         }
     }
