2fa3e72ab6d01f8e801674cdaadb1c591dca7bd9
Jack <DevJackSmith@users.noreply.github.com>
2022-06-21 14:30:38 +0200

    adding a pre-post check to FG stage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1822,9 +1822,7 @@
      */
     function addForbiddenGroveStage(message, user, user_post, hunt) {
         const was_open = user.quests.QuestForbiddenGrove.grove.is_open;
-        // 1% time => 9.6 minutes for door open and state_progress rounds to 100 (2.4 minutes for closed).
-        const imminent_state_change = (user.quests.QuestForbiddenGrove.grove.progress >= 99);
-        if (imminent_state_change) {
+        if (was_open != user_post.quests.QuestForbiddenGrove.grove.is_open) {
             if (debug_logging) {window.console.log({message: "Skipping hunt during server-side door change", user, user_post, hunt});}
             message.location = null;
         } else {
