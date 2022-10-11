44cdcd17968b4e12a265c8e1b2c60811be26be4b
Hank McCord <hymccord@gmail.com>
2022-10-11 02:21:46 -0400

    Add rules to validate user object

--- src/scripts/huntfilter.ts
+++ src/scripts/huntfilter.ts
@@ -34,6 +34,8 @@ export class IntakeRejectionEngine {
 
     private initUserRules() {
         this.userRules = [
+            new UserRequiredDifferences(),
+            new UserNumActiveTurnsIncrementedByOne(),
         ];
     }
 
@@ -64,3 +66,21 @@ class ApiResponseActiveTurn implements IRule<ApiResponse> {
         return post.active_turn === true;
     }
 }
+
+class UserRequiredDifferences implements IRule<User> {
+    readonly required_differences: (keyof User)[] = [
+        "num_active_turns",
+        "next_activeturn_seconds"
+    ]
+
+    isValid(pre: User, post: User): boolean {
+        return this.required_differences.every(key => pre[key] != post[key]);
+    }
+}
+
+class UserNumActiveTurnsIncrementedByOne implements IRule<User> {
+    isValid(pre: User, post: User): boolean {
+        return post.num_active_turns - pre.num_active_turns === 1;
+    }
+}
+
