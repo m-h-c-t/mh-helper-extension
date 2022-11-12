36102bdef559847f6de7644b6404778bff9e467f
Hank McCord <hymccord@gmail.com>
2022-11-12 17:39:59 -0500

    Change root test folder to tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,7 @@ module.exports = {
     overrides: [
         {
             files: [
-                '**/*.test.js',
+                'tests/**/*.{test,spec}.js',
             ],
             env: {
                 jest: true,
