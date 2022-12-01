1ee5e52f1daefd49d1ece210ba2c5c31177523f9
Hank McCord <hymccord@gmail.com>
2022-12-01 17:30:46 -0500

    Use new handler and remove old golem code

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -103,13 +103,6 @@ window.addEventListener("message",
             }, event.origin));
         } else if (data.mhct_log_request === 1) {
             chrome.runtime.sendMessage({"log": data});
-        } else if (data.mhct_golem_submit === 1) {
-            data.origin = event.origin;
-            chrome.runtime.sendMessage(data, (wasSubmitted) => event.source.postMessage({
-                mhct_message: 'golemSubmissionStatus',
-                submitted: wasSubmitted,
-                settings: data.settings,
-            }, event.origin));
         } else if (data.mhct_finish_load === 1) {
             showTsituLoader();
         }
