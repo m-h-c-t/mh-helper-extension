830f95d20d8a1e94bc4c67a5e84c4beb5b65db4a
Hank McCord <hymccord@gmail.com>
2022-12-14 13:23:35 -0500

    Add test for ice fortress stage

---
+++ tests/scripts/modules/stages/iceFortress.spec.ts
@@ -0,0 +1,22 @@
+import { IceFortressStager } from '@scripts/modules/stages/iceFortress'
+import { User } from '@scripts/types/hg';
+
+describe('IceFortressStager', () => {
+    it('adds boss stage (but not right now)', () => {
+        const message = {
+        } as any;
+        const stager = new IceFortressStager();
+        const userPre = {
+            quests: {
+                QuestIceFortress: {
+                   shield: {
+                        is_broken: true
+                   }
+                }
+            }
+        } as User;
+        stager.addStage(message, userPre, {} as User, {});
+
+        expect(message.stage).not.toBe("Boss");
+    })
+})
