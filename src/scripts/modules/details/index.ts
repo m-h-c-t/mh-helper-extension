92decbb9586f4b81080661ee3ffc3241681e5c97
Hank McCord <hymccord@gmail.com>
2022-12-14 13:22:06 -0500

    Add Ice Fortress detailer

--- src/scripts/modules/details/index.ts
+++ src/scripts/modules/details/index.ts
@@ -1,7 +1,9 @@
 import { type IDetailer, type IEnvironmentDetailer } from './details.types';
+import { IceFortressDetailer } from './environments/iceFortress';
 
 // Detailer for specific location
 const environmentDetailerModules: IEnvironmentDetailer[]  = [
+    new IceFortressDetailer()
 ];
 
 // Detailers that don't match on location (LNY, Halloween)
