17686ff3429a304b4a63791a4cd6591a94ecb26f
Angelo <ramuzuconcepcion@gmail.com>
2022-12-21 14:05:09 +0800

    Fix onload behavior

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -1,22 +1,22 @@
 // JS script available only within the embedded options.html page
 const mhhhOptions = [
-    {name: 'success_messages',           p: 'checked', default: true},
-    {name: 'error_messages',             p: 'checked', default: true},
-    {name: 'debug_logging',              p: 'checked', default: false},
-    {name: 'icon_timer',                 p: 'checked', default: true},
-    {name: 'horn_sound',                 p: 'checked', default: false},
-    {name: 'custom_sound',               p: 'value',   default: ''},
-    {name: 'horn_volume',                p: 'value',   default: 100},
-    {name: 'horn_volume_output',         p: 'value'},
-    {name: 'horn_alert',                 p: 'checked', default: false},
-    {name: 'horn_webalert',              p: 'checked', default: false},
-    {name: 'horn_popalert',              p: 'checked', default: false},
-    {name: 'tracking_enabled',           p: 'checked', default: true},
-    {name: 'tsitu_loader_on',            p: 'checked', default: false},
-    {name: 'tsitu_loader_offset',        p: 'value',   default: 80},
+    {name: 'success_messages', p: 'checked', default: true},
+    {name: 'error_messages', p: 'checked', default: true},
+    {name: 'debug_logging', p: 'checked', default: false},
+    {name: 'icon_timer', p: 'checked', default: true},
+    {name: 'horn_sound', p: 'checked', default: false},
+    {name: 'custom_sound', p: 'value', default: ''},
+    {name: 'horn_volume', p: 'value', default: 100},
+    {name: 'horn_volume_output', p: 'value'},
+    {name: 'horn_alert', p: 'checked', default: false},
+    {name: 'horn_webalert', p: 'checked', default: false},
+    {name: 'horn_popalert', p: 'checked', default: false},
+    {name: 'tracking_enabled', p: 'checked', default: true},
+    {name: 'tsitu_loader_on', p: 'checked', default: false},
+    {name: 'tsitu_loader_offset', p: 'value', default: 80},
     {name: 'tsitu_loader_offset_output', p: 'value'},
-    {name: 'escape_button_close',        p: 'checked', default: false},
-    {name: 'dark_mode',                  p: 'checked', default: false},
+    {name: 'escape_button_close', p: 'checked', default: false},
+    {name: 'dark_mode', p: 'checked', default: false},
 ];
 
 // Click "Save" -> store the extension's settings in chrome.storage.
@@ -31,7 +31,7 @@ document.getElementById('save').addEventListener('click', () => {
         const save_button = document.getElementById('save');
         save_button.innerText = "Options Saved! (Refreshing MH Page)";
         save_button.className = "btn btn-success";
-        setTimeout(() => {save_button.innerText = "Save"; save_button.className = "btn btn-primary";}, 2000);
+        setTimeout(() => { save_button.innerText = "Save"; save_button.className = "btn btn-primary"; }, 2000);
     });
 
     // Reload all open MH pages to apply these settings.
@@ -49,6 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .reduce((acc, prop) => (acc[prop.name] = prop.default, acc), {});
     chrome.storage.sync.get(defaultOptions, items => {
         mhhhOptions.forEach(prop => document.getElementById(prop.name)[prop.p] = items[prop.name]);
+        evaluateOptionsPageDarkMode();
         // Display the numeric values of the range-input sliders.
         document.getElementById('horn_volume_output').value = items.horn_volume;
         document.getElementById('tsitu_loader_offset_output').value = items.tsitu_loader_offset;
@@ -84,6 +85,8 @@ document.querySelector('#dark_mode').addEventListener('click', () => {
 
 //load dark mode
 const evaluateOptionsPageDarkMode = () => {
-    if (document.querySelector('#dark_mode').checked === true) document.querySelector('body.options')?.classList.add('dark-mode');
-    else document.querySelector('body.options')?.classList.remove('dark-mode');
-}; evaluateOptionsPageDarkMode();
+    const bodyEl = document.querySelector('body.options');
+
+    if (document.querySelector('#dark_mode').checked === true) bodyEl?.classList.add('dark-mode');
+    else bodyEl?.classList.remove('dark-mode');
+};
