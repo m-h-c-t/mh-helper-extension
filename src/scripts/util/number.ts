d6801a1ea4e0900e276b483545572a741c33b5e9
Hank McCord <hymccord@gmail.com>
2023-09-07 17:09:06 -0400

    Add util to parse numbers that may contain commas

---
+++ src/scripts/util/number.ts
@@ -0,0 +1,18 @@
+
+/**
+ * Gets a number from a Hg stringy number (may contain commas).
+ *
+ * @param value A number that can be a string with commas.
+ * @returns The parsed number.
+ */
+export function parseHgInt(value: string | number): number {
+    if (value == null) {
+        throw new Error('Cannot parse undefined value');
+    }
+
+    if (typeof value === 'number') {
+        return value;
+    }
+
+    return parseInt(value.replace(/,/g, ""), 10);
+}
