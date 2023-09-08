0ad96f5450855f244afce13521d7bc6e444ef9eb
Hank McCord <hymccord@gmail.com>
2023-09-08 17:26:47 -0400

    Add constants class for better organization

---
+++ src/scripts/util/constants.ts
@@ -0,0 +1,8 @@
+
+export class CustomConvertibleIds {
+    public static readonly KingsMiniPrizePack: number = 130008;
+}
+
+export class EventDates {
+    public static readonly KingsGiveawayEndDate: Date = new Date("2023-10-03T15:00:00Z");
+}
