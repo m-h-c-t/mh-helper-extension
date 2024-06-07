62ac69d758beb84b6eb9eebc955f68bba0142b21
Hank McCord <hymccord@gmail.com>
2024-06-07 13:09:21 -0400

    Update KGA tests

--- src/scripts/util/time.ts
+++ src/scripts/util/time.ts
@@ -15,3 +15,17 @@ export function getUnixTimestamp(): number {
 export function hasEventEnded(eventEndDate: Date) {
     return Date.now() > eventEndDate.getTime();
 }
+
+
+/**
+ * Adds the specified number of days to a given date.
+ *
+ * @param date - The date to which the days will be added.
+ * @param days - The number of days to add.
+ * @returns A new Date object representing the updated date.
+ */
+export function addDays(date: Date, days: number): Date {
+    const result = new Date(date);
+    result.setDate(date.getDate() + days);
+    return result;
+}
