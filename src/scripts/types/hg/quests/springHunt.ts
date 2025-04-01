526c8235b4c90e16caf32100c5c52c6dd9b17fca
Rob Wilson <aardwolf98@gmail.com>
2025-04-01 13:58:14 -0400

    removed SEH details no longer used

--- src/scripts/types/hg/quests/springHunt.ts
+++ src/scripts/types/hg/quests/springHunt.ts
@@ -1,8 +1,6 @@
 import {z} from "zod";
 
 export const questSpringHuntSchema = z.object({
-    charge_quantity: z.string(),
-    charge_doubler: z.union([z.literal('active'), z.unknown()]),
 });
 
 export type QuestSpringHunt = z.infer<typeof questSpringHuntSchema>;
