59db65277afa9be749ca48ba36aa9ff295dce5db
Hank McCord <hymccord@gmail.com>
2025-08-12 17:43:36 -0400

    Fix linting errors

--- src/scripts/modules/details/global/pillage.ts
+++ src/scripts/modules/details/global/pillage.ts
@@ -1,13 +1,14 @@
 import type {JournalMarkup, User} from '@scripts/types/hg';
+import type {IntakeMessage} from '@scripts/types/mhct';
 import type {IDetailer} from '../details.types';
 
 export class PillageDetailer implements IDetailer {
     /**
      * Track whether a FTC resulted in a pillage, and if so, the damage dealt.
      */
-    addDetails(message: any, userPre: User, userPost: User, journal: JournalMarkup): {} | undefined {
+    addDetails(message: IntakeMessage, userPre: User, userPost: User, journal: JournalMarkup): Record<PropertyKey, unknown> | undefined {
         if (message.attracted && !message.caught && journal.render_data.css_class.includes('catchfailuredamage')) {
-            const match = journal.render_data.text.match(/Additionally, .+ ([\d,]+) .*(gold|bait|points)/);
+            const match = /Additionally, .+ ([\d,]+) .*(gold|bait|points)/.exec(journal.render_data.text);
             if (match && match.length === 3) {
                 return {
                     pillage_amount: parseInt(match[1].replace(/,/g,''), 10),
