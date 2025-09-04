76c2488f25727c9bb73358d4954238382cae63f6
Hank McCord <hymccord@gmail.com>
2025-09-04 10:28:57 -0400

    Add Afterwood Acres stages for blight tiers

--- src/scripts/types/hg/quests/afterwordAcres.ts
+++ src/scripts/types/hg/quests/afterwordAcres.ts
@@ -3,6 +3,8 @@ import {z} from "zod";
 const blightTiers = ["tier_1", "tier_2", "tier_3", "tier_4"] as const;
 const blightTierSchema = z.enum(blightTiers);
 
+export type BlightTier = z.infer<typeof blightTierSchema>;
+
 export const questAfterwordAcresSchema = z.object({
     blight_tier: blightTierSchema,
 });
