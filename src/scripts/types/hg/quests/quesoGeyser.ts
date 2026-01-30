04a647de29e58908b3d6f22f4636ffaf2b597a20
Hank McCord <hymccord@gmail.com>
2026-01-30 13:12:55 -0500

    fix(quesoGeyser): update state schema to include all possible states

--- src/scripts/types/hg/quests/quesoGeyser.ts
+++ src/scripts/types/hg/quests/quesoGeyser.ts
@@ -1,12 +1,18 @@
 import { z } from 'zod';
 
 export const QuesoGeyserStates = ['collecting', 'corked', 'eruption', 'claim'] as const;
-const quesoGeyserStateSchema = z.enum(QuesoGeyserStates);
+const quesoGeyserNonCollectingStateSchema = z.enum(['corked', 'eruption', 'claim']);
 
-export const questQuesoGeyserSchema = z.object({
-    state: quesoGeyserStateSchema,
-    state_name: z.string(),
-});
+export const questQuesoGeyserSchema = z.discriminatedUnion('state', [
+    z.object({
+        state: z.literal('collecting'),
+        state_name: z.null(),
+    }),
+    z.object({
+        state: quesoGeyserNonCollectingStateSchema,
+        state_name: z.string(),
+    }),
+]);
 
 export type QuesoGeyserState = typeof QuesoGeyserStates[number];
 export type QuestQuesoGeyser = z.infer<typeof questQuesoGeyserSchema>;
